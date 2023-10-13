3f7ca10b8424698e2cfd2516077671d5b92715a2
Christoph Kronberger <christoph.kronberger@ebcont.com>
2023-10-13 14:29:39 +0200

    Changes by kristijan.mitrovic

---
+++ java/src/main/java/ScoreNames.java
@@ -0,0 +1,8 @@
+public class ScoreNames {
+    public static final String[] SCORE_NAMES = {"Love", "Fifteen", "Thirty", "Forty"};
+
+
+    static String getScoreName(int score) {
+        return SCORE_NAMES[score];
+    }
+}
